3777d0ec2b979714d0e2cd022ae3d5237442f940
JonathanP123yay <165610530+JonathanP123yay@users.noreply.github.com>
2026-04-20 20:25:44 -0400

    Enhance command unavailability message
    Updated the command's embed message to provide more context about the unavailability of the command.

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -3,13 +3,13 @@ import { CommandInteraction, EmbedBuilder } from 'discord.js'
 export async function commands (interaction:CommandInteraction) {
   const embed: EmbedBuilder = new EmbedBuilder()
     .setColor(0xFF5454)
-    .setTitle('This command is currently unavailable.')
+    .setTitle('This command is currently unavailable. (Check back later!)')
     .setThumbnail('https://starmanthegamer.com/icon.png')
     .addFields(
-      { name: 'What happened?', value: 'Polytoria has revamped their entire website, and we rely on APIs to access the information we serve to you. With those APIs currently being unavailable, we cannot serve you the requested command.' },
+      { name: 'What happened?', value: 'Polytoria has revamped their entire website, and we rely on APIs (Application Programming Interface) to access the information we serve to you. With those APIs currently being unavailable, we cannot serve you the requested command.' },
       { name: 'When will this command return?', value: 'In the future when the API for this command returns.', inline: true }
     )
-    .setFooter({ text: 'Thank you for your patience.', iconURL: 'https://starmanthegamer.com/icon.png' })
+    .setFooter({ text: 'Thank you for your patience!', iconURL: 'https://starmanthegamer.com/icon.png' })
 
   await interaction.reply({ embeds: [embed] })
 }
